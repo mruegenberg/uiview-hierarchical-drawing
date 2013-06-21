0af788a13714a375fbd4d53b314998ed395e9446
Marcel Ruegenberg <subw@dustlab.com>
2013-06-22 00:15:22 +0200

    Added code.

---
+++ UIView+HierarchicalDrawing.h
@@ -0,0 +1,15 @@
+//
+//  UIView+HierarchicalDrawing.h
+//  Classes
+//
+//  Created by Marcel Ruegenberg on 12.10.12.
+//  Copyright (c) 2012 Dustlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (HierarchicalDrawing)
+
+- (void)drawHierarchy;
+
+@end
---
+++ UIView+HierarchicalDrawing.m
@@ -0,0 +1,46 @@
+//
+//  UIView+HierarchicalDrawing.m
+//  Classes
+//
+//  Created by Marcel Ruegenberg on 12.10.12.
+//  Copyright (c) 2012 Dustlab. All rights reserved.
+//
+
+#import "UIView+HierarchicalDrawing.h"
+#import "TimetableView.h"
+
+@implementation UIView (HierarchicalDrawing)
+
+- (void)drawHierarchy {    
+    [self layoutSubviews];
+    
+    CGContextRef c = UIGraphicsGetCurrentContext();
+    CGContextSaveGState(c);
+    
+    CGContextTranslateCTM(c, self.frame.origin.x, self.frame.origin.y);
+    
+    // FIXME: this is a hack
+    if([self isKindOfClass:[TimetableView class]])
+        [self performSelector:@selector(setPatternAsBg)];
+    if([self isKindOfClass:[UIScrollView class]]) {
+        UIScrollView *sSelf = (UIScrollView *)self;
+        CGContextTranslateCTM(c, (-1) * sSelf.contentOffset.x, (-1) * sSelf.contentOffset.y);
+    }
+    if(self.backgroundColor != nil) {
+        CGContextSetFillColorWithColor(c, [self.backgroundColor CGColor]);
+        CGContextFillRect(c, self.bounds);
+    }
+    
+    [self drawRect:self.bounds];
+    
+    if(self.clipsToBounds) CGContextClipToRect(c, self.bounds);
+    
+    for(UIView *v in self.subviews) {
+        if(v.hidden) continue;
+        [v drawHierarchy];
+    }
+    CGContextRestoreGState(c);
+}
+
+
+@end
